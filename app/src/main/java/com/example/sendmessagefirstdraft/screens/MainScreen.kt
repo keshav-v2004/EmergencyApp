4beef2fdb1271bbf9a667c59d4505e8684faaa01
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2024-08-11 18:30:53 +0530

    alertDialog implemented completed.Now DB implementation WILL start

--- app/src/main/java/com/example/sendmessagefirstdraft/screens/MainScreen.kt
+++ app/src/main/java/com/example/sendmessagefirstdraft/screens/MainScreen.kt
@@ -1,6 +1,5 @@
 package com.example.sendmessagefirstdraft.screens
 
-import android.app.AlertDialog
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -12,22 +11,23 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,6 +36,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.sendmessagefirstdraft.R
 import com.example.sendmessagefirstdraft.navigation.Screens
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen(
@@ -43,15 +44,21 @@ fun MainScreen(
     navController: NavController,
 
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .statusBarsPadding()
-            .fillMaxSize()
-    ) {
-        Logos(viewModel , navController)
+    val snackbarHost = remember{ SnackbarHostState() }
 
-        BelowLogos()
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHost) }
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(it)
+                .statusBarsPadding()
+                .fillMaxSize()
+        ) {
+            Logos(viewModel, navController, snackbarHost)
+            BelowLogos()
+        }
     }
 }
 
@@ -60,14 +67,62 @@ fun MainScreen(
 fun Logos(
     viewModel: MainScreenViewModel,
     navController: NavController,
+    snackbarHostState: SnackbarHostState,
 ) {
+
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable._6zvtnln3toz309wttldiwp_25),
+            contentDescription = null,
+            modifier = Modifier
+                .combinedClickable(
+                    onClick = { navController.navigate(Screens.WhatsappScreen.name) },
+                    onLongClick = {
+                        viewModel.updateDialogState(
+                            viewModel.showWhatsappDialog,
+                            Screens.WhatsappScreen.name
+                        )
+                    }
+                )
+        )
+
+
+        Image(painter = painterResource(
+            id = R.drawable.white_exclamation_mark_sign_red_circle_isolated_white_background_120819_332
+        ),
+            contentDescription = null,
+            modifier = Modifier
+                .combinedClickable(
+                    onClick = { navController.navigate(Screens.SmsScreen.name) },
+                    onLongClick = {
+                        viewModel.updateDialogState(viewModel.showSmsDialog, Screens.SmsScreen.name)
+                    }
+                )
+        )
+
+    }
+
+
 
     if (viewModel.showSmsDialog) {
         AlertDialog(
             onDismissRequest = { viewModel.updateDialogState(viewModel.showSmsDialog , Screens.SmsScreen.name) },
             confirmButton = {
                 Button(
-                    onClick = { /*TODO*/ }
+                    onClick = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("sending")
+                        }
+                    }
                 ) {
                     Text("confirm")
                 }
@@ -84,12 +139,17 @@ fun Logos(
         )
     }
 
-    if (viewModel.showWhatsappDialog) {
+    else if (viewModel.showWhatsappDialog) {
         AlertDialog(
             onDismissRequest = { viewModel.updateDialogState(viewModel.showWhatsappDialog , Screens.WhatsappScreen.name) },
             confirmButton = {
                 Button(
-                    onClick = { /*TODO*/ }
+                    onClick = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("sending")
+                        }
+                    }
+                        
                 ) {
                     Text("confirm")
                 }
@@ -108,40 +168,7 @@ fun Logos(
 
 
 
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
 
-    ) {
-
-
-        Image(
-            painter = painterResource(id = R.drawable._6zvtnln3toz309wttldiwp_25),
-            contentDescription = null,
-            modifier = Modifier
-                .combinedClickable(
-                    onClick = {navController.navigate(Screens.WhatsappScreen.name)},
-                    onLongClick = {
-                        viewModel.updateDialogState(viewModel.showWhatsappDialog , Screens.WhatsappScreen.name)
-                    }
-                )
-        )
-
-
-        Image(painter = painterResource(
-            id = R.drawable.white_exclamation_mark_sign_red_circle_isolated_white_background_120819_332),
-            contentDescription = null,
-            modifier = Modifier
-                .combinedClickable(
-                    onClick = {navController.navigate(Screens.SmsScreen.name)},
-                    onLongClick = {
-                        viewModel.updateDialogState(viewModel.showSmsDialog , Screens.SmsScreen.name)
-
-                    }
-                )
-        )
-    }
 }
 
 @Composable
